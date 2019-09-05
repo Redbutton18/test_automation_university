93ac35fadb6bfcdaa6b4cbf3285faa9ef3583cbf
MnykhAnton <mnikh.anton@gmail.com>
2019-09-05 15:59:21 +0300

    calculating Average Test Score added

---
+++ src/chapter3/AverageTestScore.java
@@ -0,0 +1,28 @@
+package chapter3;
+
+import java.util.Scanner;
+
+public class AverageTestScore {
+    public static void main(String[] args) {
+        //Initialize what we knew
+        int numberOfTests = 4;
+        int numberOfStudents = 24;
+
+        Scanner scanner = new Scanner(System.in);
+
+        //Process all students
+        for(int i = 0; i<numberOfStudents; ++i){
+
+            double total = 0;
+            for (int j=0; j<numberOfTests; ++j){
+                System.out.println("Enter the score of the test #" + (j+1));
+                double score = scanner.nextDouble();
+                total = total + score;
+            }
+            double average = total/numberOfTests;
+            System.out.println("The test average for student #" + (i+1) + " is " + average);
+        }
+        scanner.close();
+
+    }
+}
